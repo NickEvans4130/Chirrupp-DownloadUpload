205925c4f591b28fcde6f2bc1756ed69162f4bdc
NickEvans4130 <105942777+NickEvans4130@users.noreply.github.com>
2022-06-28 17:21:55 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# importing functions from previous bits of code
+
+from xeno_download import Download_Data
+from google_upload import upload_basic
+
+# running functions
+
+if __name__ == '__main__':
+  Download_Data()
+  upload_basic()
+  
